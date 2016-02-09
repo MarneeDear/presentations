e8c46d5975ce510ce5f4837510dc94110db43099
Marnee <marnee.dearman@gmail.com>
2016-02-08 22:46:17 -0700

    moved stuff around

--- python/settings/graphene.py
+++
@@ -1,2 +0,0 @@
-__author__ = 'Marnee Dearman'
-DATABASE_URL = 'http://TUPLE:WoPZRGjRoz5JwhDP1trU@tuple.sb02.stations.graphenedb.com:24789/db/data/'
--- python/settings/local.py
+++
@@ -1,2 +0,0 @@
-__author__ = 'Marnee Dearman'
-DATABASE_URL = 'http://localhost:7474/db/data/'
