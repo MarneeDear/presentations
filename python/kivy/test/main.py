2b69eec225ae1c95df0ceb628f5117fa329370f9
Marnee <marnee.dearman@gmail.com>
2016-02-14 17:30:16 -0700

    a few minor changes to the py2neo presentation and adding a possible talk on Kivy (started)

---
+++ python/kivy/test/main.py
@@ -0,0 +1,16 @@
+__author__ = 'Marnee Dearman'
+import kivy
+kivy.require('1.0.6') # replace with your current kivy version !
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class MyApp(App):
+
+    def build(self):
+        return Label(text='Hello world')
+
+
+if __name__ == '__main__':
+    MyApp().run()
