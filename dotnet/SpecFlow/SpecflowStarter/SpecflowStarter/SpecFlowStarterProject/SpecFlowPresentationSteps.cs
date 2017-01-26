dc1be4379c37946662abafc638d620d752630828
Marnee <marnee.dearman@gmail.com>
2017-01-25 23:05:23 -0700

    I guess I accidentally removed these files.

---
+++ dotnet/SpecFlow/SpecflowStarter/SpecflowStarter/SpecFlowStarterProject/SpecFlowPresentationSteps.cs
@@ -0,0 +1,37 @@
+﻿//using system;
+//using techtalk.specflow;
+//using techtalk.specflow.assist;
+//using presentation;
+//using microsoft.visualstudio.testtools.unittesting;
+
+
+//namespace specflowstarterproject
+//{
+//	[binding]
+//	public class specflowpresentationsteps
+//	{
+//		[given(@"user marnee is presenting specflow")]
+//		public void givenusermarneeispresentingspecflow(table table)
+//		{
+//			presenter presenter = table.createinstance<presenter>();
+//			scenariocontext.current["presenter"] = presenter;
+//		}
+
+//		[when(@"marnee gives the talk to the group")]
+//		public void whenmarneegivesthetalktothegroup(table table)
+//		{
+//			presenter presenter = (presenter)scenariocontext.current["presenter"];
+//			group group = table.createinstance<group>();
+//			presenter.givepresentation(ref group);
+//			scenariocontext.current["group"] = group;
+//		}
+
+//		[then(@"we learn about behavior driven development and specflow")]
+//		public void thenwelearnaboutbehaviordrivendevelopmentandspecflow()
+//		{
+//			group group = (group)scenariocontext.current["group"];
+//			assert.isfalse(group.welearnedallthethings);
+//		}
+
+//	}
+//}
