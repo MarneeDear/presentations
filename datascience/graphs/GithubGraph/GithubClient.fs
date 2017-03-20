3ae45ab6187f5aa64d6a216ea6499a3d8a8adb73
Marnee <marnee.dearman@gmail.com>
2017-03-19 22:21:21 -0700

    Moved teh github password into a secrets file. Refactored to use the new app settings.

--- datascience/graphs/GithubGraph/GithubClient.fs
+++ datascience/graphs/GithubGraph/GithubClient.fs
@@ -4,11 +4,14 @@ open System
 open FSharp.Data
 open Newtonsoft.Json
 open FSharp.Data.HttpRequestHeaders
+open FSharp.Configuration
+
+type Settings = AppSettings<"App.config">
 
 let githubOrgURL orgLogin = sprintf "https://api.github.com/orgs/%s?per_page=100" orgLogin
 let githubUserURL userLogin = sprintf "https://api.github.com/users/%s?per_page=100" userLogin
-let githubOrgMembersURL orgLogin = sprintf "https://api.github.com/orgs/%s/members?per_page=100" orgLogin
-let githubUserOrgsURL userLogin = sprintf "https://api.github.com/users/%s/orgs?per_page=100" userLogin
+let githubOrgMembersURL orgLogin page = sprintf "https://api.github.com/orgs/%s/members?per_page=100&page=%i" orgLogin page 
+let githubUserOrgsURL userLogin page rando = sprintf "https://api.github.com/users/%s/orgs?per_page=100&page=%i&rando=%s" userLogin page rando
 
 type OrgResponse = JsonProvider<"org.json">
 type UserReponse = JsonProvider<"user.json">
@@ -19,7 +22,12 @@ let test = Http.RequestString("https://google.com")
 
 let getGithubData url =
     Http.RequestString(url = url, httpMethod = HttpMethod.Get, 
-        headers = [BasicAuth "marneedear" "60ab7b0a6bcc5912ebd2663588325a43785bbe86";Accept HttpContentTypes.Json; UserAgent "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"]
+        headers = [BasicAuth "marneedear" Settings.GithubToken; Accept HttpContentTypes.Json; UserAgent "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"]
+    )
+
+let getGithubDataPage url =
+    Http.Request(url = url, httpMethod = HttpMethod.Head, 
+        headers = [BasicAuth "marneedear" Settings.GithubToken; Accept HttpContentTypes.Json; UserAgent "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"]
     )
 
 let getGithubOrganization orgLogin =
@@ -28,11 +36,19 @@ let getGithubOrganization orgLogin =
 let getGithubUser userLogin =
     getGithubData (githubUserURL userLogin)
 
-let getGithubOrgMembers orgLogin =
-    getGithubData (githubOrgMembersURL orgLogin)
+let rando = Random().Next(100, 10000).ToString()
 
-let getGithubUserOrgs userLogin = 
-    getGithubData (githubUserOrgsURL userLogin)
+let pageNumbersList orgLogin =
+    let response = getGithubDataPage (githubOrgMembersURL orgLogin 4908)
+    let lastLink = new System.Uri((response.Headers.Item "Link").Split([| ',' |]).[1].Split([| ';' |]).[0].Replace(">", "").Replace("<", ""))
+    let lastPage = int(lastLink.Query.Split('&').[1].Split('=').[1]) //.Split('=').[1])
+    [1 .. lastPage]
+
+let getGithubOrgMembers orgLogin page =
+    getGithubData (githubOrgMembersURL orgLogin page)
+
+let getGithubUserOrgs userLogin page = 
+    getGithubData (githubUserOrgsURL userLogin page "")
 
 
 let getOrgRecord orgLogin : BuildGraph.GitHubEntity =
@@ -63,9 +79,10 @@ let getUserRecord userLogin : BuildGraph.GitHubEntity =
         Properties = properties
     }
 
-let getOrgMembersList orgLogin : BuildGraph.GitHubEntity seq = 
-    let response = getGithubOrgMembers orgLogin |> OrgMembersReponse.Parse
+let getOrgMembersList orgLogin page : BuildGraph.GitHubEntity seq = 
+    let response = (getGithubOrgMembers orgLogin page) |> OrgMembersReponse.Parse
     response |> Seq.map (fun u -> getUserRecord u.Login) //|> Seq.map getUserRecord
 
 //let getUserOrgs
 //60ab7b0a6bcc5912ebd2663588325a43785bbe86
+
--- datascience/graphs/GithubGraph/Program.fs
+++ datascience/graphs/GithubGraph/Program.fs
@@ -3,16 +3,31 @@
 // Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
-let buildGithubGraph orgLogin = 
-    BuildGraph.createEntity (GithubClient.getOrgRecord orgLogin) |> ignore
-        
-    GithubClient.getOrgMembersList orgLogin    
-    |> Seq.iter (fun user -> 
-                    BuildGraph.createEntity user
-                    BuildGraph.createOrganizationMembership user.Properties.Login orgLogin
-                    BuildGraph.createLocation user.Properties.Location
-                    BuildGraph.createMemberLocation user.Properties.Login user.Properties.Location
-                    )
+let ``create the organization`` orgLogin =
+    BuildGraph.createEntity (GithubClient.getOrgRecord orgLogin)
+
+let ``get the organization members list`` orgLogin page =
+    GithubClient.getOrgMembersList orgLogin page
+    
+let ``create the organization member and location graph`` user orgLogin =
+    BuildGraph.createEntity user
+    BuildGraph.createOrganizationMembership user.Properties.Login orgLogin
+    BuildGraph.createLocation user.Properties.Location
+    BuildGraph.createMemberLocation user.Properties.Login user.Properties.Location
+
+let ``process each page of the members`` orgLogin page =
+    ``get the organization members list`` orgLogin page
+    |> Seq.iter (fun user -> ``create the organization member and location graph`` user orgLogin)
+
+let ``feed the graph`` orgLogin = 
+    let pagesList = GithubClient.pageNumbersList orgLogin
+    ``create the organization`` orgLogin
+    //pagesList |> List.iter
+    pagesList |> List.iter (``process each page of the members`` orgLogin)
+
+//    ``get the organization members list`` orgLogin 1
+//    |> Seq.iter (fun user -> ``create the organization member and location graph`` user orgLogin)
+    ()
 
 //    |> Seq.map (fun user -> 
 //                    BuildGraph.createEntity user
@@ -20,7 +35,7 @@ let buildGithubGraph orgLogin =
 //    |> Seq.iter (fun user -> 
 //                    BuildGraph.createOrganizationMembership user.Properties.Login orgLogin
 //                    )
-    ()
+
 
 [<EntryPoint>]
 let main argv = 
@@ -29,7 +44,9 @@ let main argv =
     printfn "%s" "Why hello there. Let's build a graph."
 //    Environment.NewLine |> printfn "%s"
     printfn "%s" "O-[R]->O"
+    projects |> Array.iter (printfn "%s")
     Environment.NewLine |> printfn "%s"
+    printfn "%s" "*************************************"
 //    printfn "%s %s" "I am going to look for these Github Organizations for you." Environment.NewLine
 //    projects |> Array.iter (printfn "%s")
 //    projects |> Array.item 0 |> (printfn "Trying %s%s" Environment.NewLine)
@@ -41,7 +58,7 @@ let main argv =
 //    projects |> Array.item 0 |> GithubClient.getOrgRecord |> printfn "RECORD %A"
 //    Environment.NewLine |> printfn "%s"
     printfn "%s" "Please wait while I build the graph. This is going to be awesome."
-    projects |> Array.iter buildGithubGraph //builds members
+    projects |> Array.iter ``feed the graph`` //builds members and their locations
     //TODO build locations graph
     //TODO do some analysis
     Environment.NewLine |> printfn "%s"
